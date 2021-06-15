92e028f120c465bb33e976595af9b8df662fd620
bernicemercy <84614692+bernicemercy@users.noreply.github.com>
2021-06-15 16:35:37 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,28 @@
+# to divide multiply and add
+a = 123
+b =789
+c = 710
+
+print(a/10)
+print(b*50)
+print(c+60)
+
+# to change the value at an index of a string
+s = "Hello"
+s = list(s)
+s[2] = "G"
+string = ""
+for ele in s:
+    string += ele
+print(string)
+
+
+# int to float
+
+x = 22
+print(float(x))
+
+# float to int
+
+f = 123.234
+print(int(f))
